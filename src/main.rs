df341d46342bb18387212687954cffcb7988bc76
dlaststark <dlaststark@gmail.com>
2023-03-18 20:47:55 +0530

    added functionality to read feed.xml

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use std::error::Error;
 use reqwest;
+use rss::Channel;
 
 #[tokio::main]
-async fn main() {
-    let result = reqwest::get("https://www.hindustantimes.com/feeds/rss/cricket/ipl/rssfeed.xml").await;
-    println!("{:?}", result);
+async fn main() -> Result<(), Box<dyn Error>> {
+    let response = reqwest::get("https://www.hindustantimes.com/feeds/rss/latest/rssfeed.xml".to_string()).await?;
+    //println!("{:?}", response);
+
+    let content = response.bytes().await?;
+    //println!("{:?}", content);
+
+    let rss = Channel::read_from(&content[..])?;
+    //println!("{:?}", rss);
+
+    for item in rss.items() {
+        println!("Title: {:?}", item.title());
+        println!("Description: {:?}", item.description());
+    }
+
+    Ok(())
 }
